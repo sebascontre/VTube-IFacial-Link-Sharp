0b2d6d016a9d2a4ebf18db974c66d6f8461528bd
SebasContre <sebascontre@users.noreply.github.com>
2025-08-21 12:08:35 -0400

    Set a minimun size for the window

--- VTube-IFacial-Link/MainWindow.xaml.cs
+++ VTube-IFacial-Link/MainWindow.xaml.cs
@@ -12,9 +12,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +28,37 @@ namespace VTube_IFacial_Link
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private delegate IntPtr WindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+
+        private const int GWLP_WNDPROC = -4;
+        private const uint WM_GETMINMAXINFO = 0x0024;
+
+        private WindowProc _newWndProcDelegate;
+        private IntPtr _oldWndProc;
+
         public MainWindow()
         {
             this.InitializeComponent();
@@ -33,6 +66,32 @@ namespace VTube_IFacial_Link
 
             this.ExtendsContentIntoTitleBar = true;
             SetTitleBar((this.Content as Views.MainPage).AppTitleBar);
+
+            SetupWindowMinSize();
+        }
+
+        private void SetupWindowMinSize()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            if (hWnd == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Could not get window handle.");
+            }
+
+            _newWndProcDelegate = new WindowProc(NewWindowProc);
+            _oldWndProc = SetWindowLongPtr(hWnd, GWLP_WNDPROC, Marshal.GetFunctionPointerForDelegate(_newWndProcDelegate));
+        }
+
+        private IntPtr NewWindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            if (msg == WM_GETMINMAXINFO)
+            {
+                MINMAXINFO mmi = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                mmi.ptMinTrackSize.x = 720;
+                mmi.ptMinTrackSize.y = 450;
+                Marshal.StructureToPtr(mmi, lParam, true);
+            }
+            return CallWindowProc(_oldWndProc, hWnd, msg, wParam, lParam);
         }
     }
 }
